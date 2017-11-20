2e236f4971640a9bd632e53f040228f83d8ef90c
Prashant Singh <mprashantks@gmail.com>
2017-11-21 00:39:52 +0530

    Indentation in reverse-number program

--- reverse-number/reverse-number.cpp
+++ reverse-number/reverse-number.cpp
@@ -2,40 +2,39 @@
 
 using namespace std;
 
-int reverse(int num, int rev) {
+int reverse (int num, int rev) {
     // Using recursion
-    if(num==0)
+    if (num==0)
         return rev;
 
-    rev=rev*10+num%10;
+    rev = rev*10 + num%10;
     reverse(num/10, rev);
 
     // Using iteration
     // int rev=0;
     //
-    // while(num!=0) {
-    //     rev=rev*10+num%10;
-    //     num/=10;
+    // while (num!=0) {
+    //     rev = rev*10 + num%10;
+    //     num /= 10;
     // }
     // return rev;
 }
 
 int main() {
 	//code
-	int numberOfTestCases;
+	int num_test_cases;
 
-	cin>>numberOfTestCases;
-	while(numberOfTestCases > 0) {
+	cin >> num_test_cases;
+	while (num_test_cases > 0) {
 	    //Code to solve problem
 	    int num;
-	    cin>>num;
+	    cin >> num;
 
-	    while(num%10==0)
-	        num/=10;
+	    while (num%10 == 0)
+	        num /= 10;
 
-	    cout<<reverse(num, 0)<<endl;
-
-	    numberOfTestCases--;
+	    cout << reverse(num, 0) << endl;
+	    num_test_cases--;
 	}
 	return 0;
 }
